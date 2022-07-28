667e1b6d8a5f0c23f8648e7df2e7a7a17109f7d4
Topu Rayhan <75056509+topurayhan007@users.noreply.github.com>
2022-07-28 23:30:26 +0600

    Update main.js

--- main.js
+++ main.js
@@ -208,5 +208,15 @@ function defaultLocation(){
     
 }
 
+const target = document.querySelector('fiveDay')
 
+target.addEventListener('wheel', event => {
+  const toLeft  = event.deltaY < 0 && target.scrollLeft > 0
+  const toRight = event.deltaY > 0 && target.scrollLeft < target.scrollWidth - target.clientWidth
+
+  if (toLeft || toRight) {
+    event.preventDefault()
+    target.scrollLeft += event.deltaY
+  }
+})
 
